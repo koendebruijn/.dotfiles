4f59d424e9fc680f961280e0d0296320d9931947
koendebruijn <bruijnk@gmail.com>
2024-10-01 22:16:13 +0200

    add tmux & wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	color_scheme = "GruvboxDark",
+	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium", stretch = "Normal", style = "Normal" }), -- (AKA: JetBrainsMono NFM, JetBrainsMono NFM Medium) /Users/koen/Library/Fonts/JetBrainsMonoNerdFontMono-Medium.ttf, CoreText
+}
+
+return config
